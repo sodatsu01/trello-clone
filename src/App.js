75401b50c357705bb0911763667274f78e566250
sodatsu01 <darumasanga.korondaa@gmail.com>
2020-01-13 18:05:19 +0900

    fix for lint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * react app
+ * @return {String} html
+ */
 function App() {
   return (
     <div className="App">
