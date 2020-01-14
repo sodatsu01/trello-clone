453e5c6b102829cd1dc84e972895d79ab4fa518f
sodatsu01 <darumasanga.korondaa@gmail.com>
2020-01-14 14:19:49 +0900

    add board to loop

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import BoardCard from './boardcard'
 
 function Board(props) {
+  const boards = [
+    {id: 1, name: "test"},
+    {id: 2, name: "hoge"}
+  ]
   return(
     <div className="board">
       <BoardCard new={true} boardName="" />
+      {boards.map((board)=>{ return <BoardCard new={false} boardName={board.name}/>})}
     </div>
   )
 }
